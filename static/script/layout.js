51ffec115b9693835359adeca90e37c008164f76
goodaseul <gongdue3@gmail.com>
2024-02-25 19:42:01 +0900

    240225 포트폴리오 수정

--- static/script/layout.js
+++ static/script/layout.js
@@ -1,10 +1,8 @@
-//커스텀 커서
+// 커스텀 커서
 let mouseCursor = document.querySelector(".cursor");
-let navLinks = document.querySelectorAll(".link_btn"); //메뉴 링크
-//window 객체에 scroll & mouse 이벤트를 추가하고 cursor함수 실행되도록 함
+let navLinks = document.querySelectorAll(".link_btn");
 window.addEventListener("scroll", cursor);
 window.addEventListener("mousemove", cursor);
-//커스텀 커서의 left값과 top값을 커서의 XY좌표값과 일치시킴
 function cursor(e) {
     mouseCursor.style.left = e.pageX + "px";
     mouseCursor.style.top = e.pageY - scrollY + "px";
@@ -63,7 +61,6 @@ function ActiveMenu() {
 // work
 
 // 더보기
-
 const workBtn = document.querySelector(".section_work .btn_more");
 
 function btnLoad(id, cnt, btn) {
