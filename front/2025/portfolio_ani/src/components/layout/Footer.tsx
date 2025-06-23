37c34ec53e33823d5e35cd4bc83cf92e032b9c08
goodaseul <gongdue3@gmail.com>
2025-06-23 21:57:10 +0900

    add: header/footer

--- front/2025/portfolio_ani/src/components/layout/Footer.tsx
+++ front/2025/portfolio_ani/src/components/layout/Footer.tsx
@@ -1,13 +1,60 @@
-import React from "react";
+"use client";
+import React, { useState, useEffect } from "react";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import { navList } from "@/store/store";
+import styles from "./Footer.module.scss";
+import { motion, useAnimation } from "framer-motion";
 
 const Footer = () => {
-  return (
-    <footer id="footer">
-      <div className="copyright text-gray-700 dark:text-gray-300 text-center md:flex items-center justify-center">
-        ©2025 Ahram Kim.&nbsp;<p>All rights reserved.</p>
-      </div>
-    </footer>
-  );
+    const router = usePathname();
+    const controls = useAnimation();
+
+    useEffect(() => {
+        controls.start({
+            x: ["0%", "-50%"],
+            scale: 1,
+            color: "#b99dcf",
+            letterSpacing: "0.05em",
+            transition: {
+                x: {
+                    repeat: Infinity,
+                    repeatType: "loop",
+                    duration: 30,
+                    ease: "linear",
+                },
+                scale: { duration: 0.3 },
+                color: { duration: 0.3 },
+                letterSpacing: { duration: 0.3 },
+            },
+        });
+    }, [controls]);
+
+    return (
+        <footer className="">
+            <div className="inner">
+                <div className={`${styles.menus}`}>
+                    {Object.entries(navList).map(([path, { name }]) => (
+                        <Link key={path} href={path} className={`${styles.menu} text-2xl leading-normal md:text-3xl lg:text-6xl lg:leading-normal 2xl:text-8xl 2xl:leading-normal relative ${router === path ? "font-extrabold" : "font-medium"} fillText ${router === path ? "active" : ""}`} data-text={name}>
+                            {name}
+                        </Link>
+                    ))}
+                </div>
+            </div>
+
+            <div className="overflow-hidden whitespace-nowrap dark:bg-gray-900 py-2">
+                <motion.div animate={controls} className={`inline-flex select-none ${styles.ticker}`} style={{ whiteSpace: "nowrap", fontSize: "1.25rem", fontWeight: 600 }}>
+                    {Array(6)
+                        .fill(null)
+                        .map((_, i) => (
+                            <span key={i} className="mx-8 italic transition-all">
+                                ©2025 Ahram Kim. All rights reserved.
+                            </span>
+                        ))}
+                </motion.div>
+            </div>
+        </footer>
+    );
 };
 
 export default Footer;
--- front/2025/portfolio_ani/src/components/layout/HeaderMade.tsx
+++ front/2025/portfolio_ani/src/components/layout/HeaderMade.tsx
@@ -1,24 +1,29 @@
 "use client";
 
-import Link from "next/link";
-import ThemeBtn from "./ThemeBtn";
-import { MdOutlineMenu } from "react-icons/md";
-import styles from "./HeaderMade.module.scss";
 import { useEffect, useState } from "react";
-import { useTheme } from "next-themes";
-// import { usePathname } from "next/navigation";
-// import { navList } from "@/store/store";
-
+import HeaderLayout from "./HeaderLayout";
+import HeaderFullMenu from "./HeaderFullMenu";
 const HeaderMade: React.FC = () => {
-    const { theme } = useTheme();
-    // const router = usePathname();
     const [isOn, setIsOn] = useState(false);
     const [lastScrollY, setLastScrollY] = useState(0);
+    const [isVisible, setIsVisible] = useState(false);
+    useEffect(() => {
+        if (isVisible) {
+            document.body.style.overflow = "hidden";
+        } else {
+            document.body.style.overflow = "";
+        }
+
+        return () => {
+            document.body.style.overflow = "";
+        };
+    }, [isVisible]);
 
     useEffect(() => {
         let ticking = false;
 
         const handleScroll = () => {
+            if (isVisible) return;
             const currentScrollY = window.scrollY;
 
             if (!ticking) {
@@ -43,50 +48,12 @@ const HeaderMade: React.FC = () => {
         return () => window.removeEventListener("scroll", handleScroll);
     }, [lastScrollY]);
     return (
-        <header
-            id="header"
-            className={`
-            ${styles.header}
-            ${isOn ? styles.on : ""}
-            ${isOn && theme === "dark" ? styles.bgDark : ""}
-            ${isOn && theme === "light" ? styles.bgLight : ""}
-        `}
-        >
-            <div className="inner flex items-center justify-between">
-                <h1 className={`logo`}>
-                    <Link href={"/"} className={`font-extrabold italic tracking-wide text-point transition-font-size duration-300 ease-in-out ${isOn ? "text-2xl" : "text-8xl"}`}>
-                        AHRAM
-                    </Link>
-                </h1>
-                <ul className="flex items-center">
-                    <li>
-                        <ThemeBtn />
-                    </li>
-                    <li>가/A</li>
-                    <li>
-                        <button className="text-2xl">
-                            <MdOutlineMenu />
-                        </button>
-                    </li>
-                </ul>
-                {/* <div className="">
-                    {Object.entries(navList).map(([path, { name }]) => {
-                        return (
-                            <Link
-                                key={path}
-                                href={path}
-                                className={`
-                            ${router === path ? "font-extrabold" : ""}
-                                    transition-all flex align-middle relative py-1`}
-                            >
-                                {name}
-                            </Link>
-                        );
-                    })}
-                </div> */}
-            </div>
-        </header>
+        <>
+            {/* 헤더 레이아웃 컴포넌트 */}
+            <HeaderLayout isOn={isOn} isVisible={isVisible} setIsVisible={setIsVisible} />
+            {/* 햄버거 메뉴 컴포넌트 */}
+            <HeaderFullMenu isVisible={isVisible} />
+        </>
     );
 };
-
 export default HeaderMade;
