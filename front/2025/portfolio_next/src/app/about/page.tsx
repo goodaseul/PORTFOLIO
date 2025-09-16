70b18c5ae66985178de907777c315a3858d88822
Jeong Daseul <98886223+goodaseul@users.noreply.github.com>
2025-09-16 22:10:28 +0900

    fix: 임시loading

--- front/2025/portfolio_next/src/app/about/page.tsx
+++ front/2025/portfolio_next/src/app/about/page.tsx
@@ -5,18 +5,18 @@ import Skill from "@/components/about/Skill";
 import Loading from "@/components/layout/loading";
 const About = () => {
     return (
-        <div className="w-full bg-gray-100 dark:bg-black min-h-[100vh]">
-            <div className="min-h-[100vh]">
-                <Info />
-            </div>
-            <div className="inner !pb-60">
-                <Loading>
+        <Loading>
+            <div className="w-full bg-gray-100 dark:bg-black min-h-[100vh]">
+                <div className="min-h-[100vh]">
+                    <Info />
+                </div>
+                <div className="inner !pb-60">
                     <Work />
                     <Certification />
                     <Skill />
-                </Loading>
+                </div>
             </div>
-        </div>
+        </Loading>
     );
 };
 
--- front/2025/portfolio_next/src/app/project/page.tsx
+++ front/2025/portfolio_next/src/app/project/page.tsx
@@ -1,9 +1,14 @@
 import { fetchProjectData } from "@/lib/apiList";
+import Loading from "@/components/layout/loading";
 import ClientProject from "../../components/project/ClientProject";
 
 const ProjectPage = async () => {
     const projects = await fetchProjectData();
-    return <ClientProject projects={projects} />;
+    return (
+        <Loading>
+            <ClientProject projects={projects} />
+        </Loading>
+    );
 };
 
 export default ProjectPage;
