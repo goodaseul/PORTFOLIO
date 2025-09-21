073feb4c86f9ae197ca19d1b17cf7d90395f3a3d
Jeong Daseul <98886223+goodaseul@users.noreply.github.com>
2025-09-21 21:38:20 +0900

    modify: url수정

--- front/2025/portfolio_next/src/lib/apiList.ts
+++ front/2025/portfolio_next/src/lib/apiList.ts
@@ -22,7 +22,7 @@ export async function getMenu(): Promise<MenuItem[]> {
     });
 }
 
-const baseUrl = process.env.NEXT_PUBLIC_BASE_URL!;
+const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
 
 // Work 데이터 가져오기
 export const fetchWorkData = async (): Promise<(AboutItem & { id: number })[]> => {
